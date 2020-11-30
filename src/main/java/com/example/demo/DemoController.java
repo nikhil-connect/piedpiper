1530ef5757202b1ad6d838485dbde2cd78f79374
nikhil-connect <70809356+nikhil-connect@users.noreply.github.com>
2020-11-30 03:21:29 +0000

    Create DemoController.java

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+@GetMapping(value = "/check")
+	@ResponseBody
+	public String contentCheckGet () {
+		return System.currentTimeMillis()+"";
+	}
+}
